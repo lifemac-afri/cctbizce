958561f5a83d293ce35a43b91ab0e82ff01e375e
Collins <woelinamcollins@gmail.com>
2025-03-19 13:01:07 +0000

    Removed an &amp;

--- src/components/contact/ContactPageComponent.jsx
+++ src/components/contact/ContactPageComponent.jsx
@@ -74,7 +74,7 @@ const ContactPageComponent = () => {
           </h1>
           <p className="text-xl text-gray-100 max-w-2xl">
             Transform your business vision into reality with CCT Business
-            Consult &amp; Events
+            Consult & Events
           </p>
 
           <div className="flex space-x-4 mt-16">
